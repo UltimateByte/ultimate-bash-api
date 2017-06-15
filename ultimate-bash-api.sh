e2c534cee728c858469de645f2645c4d6d432d76
UltimateByte <robin.labadie@gmail.com>
2017-06-15 17:38:28 +0200

    code init

---
+++ ultimate-bash-api.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Name: Ultimate BASH API
+# Version: 2017-06-15
+# Description: Add nice pre-made functions to your bash scripts
+# Developer: Robin Labadie
+# Websites: lrob.fr | terageek.org
+
+##############
+## SETTINGS ##
+##############
+
+# Override scripts settings
+master_settings_override="on" # on/off, override settings for all scripts using the API
+
+# Sleep
+master_sleeptime="0" # Global sleeptime settings
+master_sleeptime_override="on" # on/off, force this value for all scripts using the API
+
+# Mailing
+master_mail_notifications="on" 
+master_mail_notifications_override="on" # on/off, force this value for all scripts using the API
+master_mail_address="root@localhost" # Global email alert address
+master_mail_address_override="on" # on/off, force this value for all scripts using the API
+
+# Logging
+master_logdir="$(pwd)"
+
+## Misc vars
+function_selfname="Ultimate-Bash-API" # Name of the script
+master_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))" # Name of this script file
+uba_log="${logdir}/${function_selfname}.log" # Define log name
+
+
+############
+## SCRIPT ##
+############
+
+## Settings override
+
+if [ "${master_settings_override}" == "on" ]; then
+  if [ "${master_sleeptime_override}" == "on" ]; then
+    sleeptime="${master_sleeptime}"
+  fi
+  if [ "${master_sleeptime_override}" == "on" ]; then
+  
+fi

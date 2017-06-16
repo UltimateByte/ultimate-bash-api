64b5a0c4894754851c1265560cd8a978429f2940
UltimateByte <robin.labadie@gmail.com>
2017-06-16 17:32:13 +0200

    Update ultimate-bash-api.sh

--- ultimate-bash-api.sh
+++ ultimate-bash-api.sh
@@ -35,12 +35,21 @@ uba_log="${logdir}/${function_selfname}.log" # Define log name
 ## SCRIPT ##
 ############
 
+
+# TIME
+SECONDS=0
+
 ## Settings override
 
 if [ "${master_settings_override}" == "on" ]; then
-  if [ "${master_sleeptime_override}" == "on" ]; then
-    sleeptime="${master_sleeptime}"
-  fi
-  if [ "${master_sleeptime_override}" == "on" ]; then
-  
+	if [ "${master_sleeptime_override}" == "on" ]; then
+		sleeptime="${master_sleeptime}"
+	fi
+	if [ "${master_mail_notifications_override}" == "on" ]; then
+  		mail_notifications="${master_mail_notifications}"
+	fi
+	if [ "${master_mail_address_override}" == "on" ]; then
+  		mail_address="${master_mail_address}"
+	fi
 fi
+

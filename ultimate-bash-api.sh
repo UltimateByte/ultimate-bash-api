20240773d1edb39e577e0a194e59aafe2bd2b847
UltimateByte <robin.labadie@gmail.com>
2017-07-03 16:47:33 +0200

    Starts too look like something usable

--- ultimate-bash-api.sh
+++ ultimate-bash-api.sh
@@ -5,31 +5,26 @@
 # Developer: Robin Labadie
 # Websites: lrob.fr | terageek.org
 
+## Misc vars
+api_selfname="Ultimate-Bash-API" # Name of the script
+master_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))" # Name of this script file
+master_rootdir="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))"
+uba_log="${logdir}/${function_selfname}.log" # Define log name
+
 ##############
 ## SETTINGS ##
 ##############
 
-# Override scripts settings
-master_settings_override="on" # on/off, override settings for all scripts using the API
-
 # Sleep
-master_sleeptime="0" # Global sleeptime settings
-master_sleeptime_override="on" # on/off, force this value for all scripts using the API
+sleeptime="0" # Sleeptime settings
 
 # Mailing
-master_mail_notifications="on" 
-master_mail_notifications_override="on" # on/off, force this value for all scripts using the API
-master_mail_address="root@localhost" # Global email alert address
-master_mail_address_override="on" # on/off, force this value for all scripts using the API
+mail_notifications="on" 
+mailto_address="root@localhost" # Recipient mail alert address
 
 # Logging
-master_logdir="$(pwd)"
-
-## Misc vars
-function_selfname="Ultimate-Bash-API" # Name of the script
-master_selfname="$(basename $(readlink -f "${BASH_SOURCE[0]}"))" # Name of this script file
-uba_log="${logdir}/${function_selfname}.log" # Define log name
-
+logdir="${master_rootdir}/log/" # Directory in which to save logs
+log="${logdir}/${api_selfname}.log"
 
 ############
 ## SCRIPT ##
@@ -53,3 +48,56 @@ if [ "${master_settings_override}" == "on" ]; then
 	fi
 fi
 
+fn_logging(){
+	if [ -n "${logdir}" ]&&[ ! -d "${logdir}" ]; then
+		mkdir -pv "${logdir}"
+	fi
+	# Create logfile
+	if [ -n "${log}" ]&&[ ! -f "${log}" ]; then
+		touch "${log}"
+	fi
+}
+
+# Give a form to echo messages
+fn_echo_form(){
+	echoform="$(date +%Y-%m-%d_%H:%M:%S) - ${selfname} - ${currmessage}"
+}
+
+# Simple echo with date and selfname
+# Usage fn_echo "Your Message"
+fn_echo(){
+	currmessage="$1"
+	fn_echo_form
+	echo -e "${echoform}"
+}
+
+# Echo with date and output to log at the same time
+# Usage fn_logecho "Your Message"
+fn_logecho(){
+	currmessage="$1"
+	fn_echo_form
+	echo -e "${echoform}"
+	echo -e "${echoform}" >> "${log}"
+	currlog="${currlog}${echoform}\n"
+}
+
+# Send mail notification
+# Define mailsubject to your mail subject
+# Define mailcontent to your mail body message
+fn_mail(){
+	if [ "${mailalert}" == "yes" ]; then
+		fn_logecho "[INFO] Sending mail alert to: ${mailaddress}"
+		echo -e "${mailcontent}" | mail -s "$(hostname -s) - ${mailsubject}" ${mailto_address}
+	fi
+}
+
+# Send mail containing all current log
+fn_mail_currlog(){
+	if [ "${mailalert}" == "yes" ]; then
+		fn_logecho "[INFO] Sending mail alert to: ${mailaddress}"
+		echo -e "${currlog}" | mail -s "$(hostname -s) - ${mailsubject}" ${mailto_address}
+	fi
+}
+
+# Create log files
+fn_logging
